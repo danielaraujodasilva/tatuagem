baa7f84e256b093ebe5fd245e95f35e02cb176f1
danielaraujodasilva <danielaraujodasilva@gmail.com>
2026-01-11 20:20:53 -0300

    atualizaBD

--- ficha/config/conexao.php
+++ ficha/config/conexao.php
@@ -2,13 +2,18 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$servername = "br926.hostgator.com.br";
-$username = "fran5062_clientes";
-$password = "Clientes*123";
-$dbname = "fran5062_tatuagem";
+$servername = "localhost";
+$username   = "tatu_user";
+$password   = "Daniel*123";
+$dbname     = "tatuagem_novo";
 try {
     $conn = new mysqli($servername, $username, $password, $dbname);
 } catch (mysqli_sql_exception $e) {
     echo "Erro na conexão com o banco de dados: " . $e->getMessage();
 }
+
+
+
+
+
 ?>
